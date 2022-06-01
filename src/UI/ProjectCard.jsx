a7964fb2bc8b1126702a78f5c1e382bd6b2f9fe4
Tapish sharma <tapishsharma1234@gmail.com>
2022-06-01 21:26:12 +0530

    Projects add and media query written for project page

--- src/UI/ProjectCard.jsx
+++ src/UI/ProjectCard.jsx
@@ -1,12 +1,48 @@
 import React from "react";
 import classes from "../styles/Project.module.css";
+import Button from "./Button";
+import { v4 as uuid } from "uuid";
 
-const ProjectCard = ({ id, title, desc, netlify, git, resp, img }) => {
+const techicon = (value) => {
+  if (value === "HTML") {
+    return <i className="devicon-html5-plain-wordmark colored"></i>;
+  }
+  if (value === "CSS") {
+    return <i className="devicon-css3-plain-wordmark colored"></i>;
+  }
+  if (value === "JavaScript") {
+    return <i className="devicon-javascript-plain colored"></i>;
+  }
+  if (value === "React") {
+    return <i className="devicon-react-original-wordmark colored"></i>;
+  }
+  if (value === "Redux") {
+    return <i className="devicon-redux-original colored"></i>;
+  }
+  if (value === "Bootstrap") {
+    return <i className="devicon-bootstrap-plain-wordmark colored"></i>;
+  }
+  if (value === "Tailwind") {
+    return <i className="devicon-tailwindcss-original-wordmark colored"></i>;
+  }
+};
+
+const ProjectCard = ({ id, title, desc, netlify, git, resp, img, tech }) => {
   return (
     <div className={classes.project}>
       <div className={classes.pcontainer}>
         <div className={classes.title}>
-          <h1>{id}. {title}</h1>
+          <h1>
+            {id}. {title}
+          </h1>
+          <div className={classes.action_button}>
+            <a href={netlify} target="_blanck">
+              <Button text="Demo" />
+            </a>
+            <a href={git} target="_blanck">
+              <Button text="Code" />
+            </a>
+          </div>
         </div>
         <div className={classes.content}>
           <div className={classes.desc}>
@@ -18,6 +54,11 @@ const ProjectCard = ({ id, title, desc, netlify, git, resp, img }) => {
         </div>
         <div className={classes.img}>
           <img width="100%" src={img} alt="alt text" />
+        </div>
+        <div className={classes.techicon}>
+          {tech.map((el) => (
+            <div key={uuid()}>{techicon(el)}</div>
+          ))}
         </div>
       </div>
     </div>
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,64 +1,74 @@
 import React from "react";
 import classes from "../styles/Project.module.css";
 import ProjectCard from "../UI/ProjectCard";
+import Connect from "./Connect";
+import { v4 as uuid } from "uuid";
 
 const project = [
   {
     id: 1,
     title: "Cultfit Clone",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum eaque deserunt assumenda possimus beatae velit perspiciatis distinctio obcaecati pariatur est!",
-    netlify: "https://charming-kleicha-f7531f.netlify.app/checkout.html",
+    desc: "This website provides you with healthy food and good lifestyle features. It is completely responsive with entire product flow. This was part of a group project. I learned to use bootstrap and API during this project.",
+    netlify: "https://charming-kleicha-f7531f.netlify.app/",
     git: "https://github.com/ROY-AMAN/Cult-Fit-clone",
-    resp: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum eaque deserunt assumenda possimus beatae velit perspiciatis distinctio obcaecati pariatur est!",
+    resp: "I build Eat page from where you can order food. Food can be added to cart from where payment can be made. A veg filter has been added form all vegeterians. I also created checkout page.",
     img: "/cult1.png",
+    tech: ["HTML", "CSS", "JavaScript", "Bootstrap"],
   },
   {
     id: 2,
     title: "OverStock Clone",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum eaque deserunt assumenda possimus beatae velit perspiciatis distinctio obcaecati pariatur est!",
-    netlify: "https://profound-praline-6eee04.netlify.app/cart.html ",
+    desc: "This website provides you with personal and home decoration items. This website is responsive with entire product flow. This was part of a group project. I learned to use Dom manipulation during this project",
+    netlify: "https://profound-praline-6eee04.netlify.app/ ",
     git: "https://github.com/amananshul/Over_Stock_project",
-    resp: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum eaque deserunt assumenda possimus beatae velit perspiciatis distinctio obcaecati pariatur est!",
+    resp: "I build jewellery and furniture page. I included sorting by price and few filter method. This makes good user experience. I also made the footer component.",
     img: "/overstock1.png",
+    tech: ["HTML", "CSS", "JavaScript"],
   },
   {
     id: 3,
-    title: "Google Clock clone",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum eaque deserunt assumenda possimus beatae velit perspiciatis distinctio obcaecati pariatur est!",
+    title: "Timer clone",
+    desc: "This can be used a both stopwatch and timer. I learn to use custom componets during this project",
     netlify: "https://incredible-figolla-9ff290.netlify.app/",
     git: "https://github.com/masai-course/tapish_fw16_560/tree/master/Unit%205/sprint-1/day-3/assignments",
-    resp: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum eaque deserunt assumenda possimus beatae velit perspiciatis distinctio obcaecati pariatur est!",
+    resp: "This was create using custom hooks and react-redux.",
     img: "/clock.png",
+    tech: ["HTML", "CSS", "JavaScript", "React", "Redux"],
   },
   {
     id: 4,
     title: "Max Faxhion Clone",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum eaque deserunt assumenda possimus beatae velit perspiciatis distinctio obcaecati pariatur est!",
+    desc: "This website provides you with clothing. This website is responsive with entire product flow. This was part of a group project.",
     netlify: "https://leafy-sorbet-dccc4d.netlify.app/",
     git: " https://github.com/kakashi10-23/Max-clone",
-    resp: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum eaque deserunt assumenda possimus beatae velit perspiciatis distinctio obcaecati pariatur est!",
+    resp: "I build Home page and cart page of this website. I learn to use tailwind library and moch-server during this project. I also made the footer component",
     img: "/max1.png",
+    tech: ["HTML", "CSS", "JavaScript", "React", "Tailwind"],
   },
 ];
 
 const Project = () => {
   return (
-    <section className={classes.project}>
-      <div className={classes.project_container}>
-        {project.map(({ id, title, desc, netlify, git, resp, img }) => (
-          <ProjectCard
-            key={id}
-            id={id}
-            title={title}
-            desc={desc}
-            netlify={netlify}
-            git={git}
-            resp={resp}
-            img={img}
-          />
-        ))}
-      </div>
-    </section>
+    <>
+      <section className={classes.project}>
+        <div className={classes.project_container}>
+          {project.map(({ id, title, desc, netlify, git, resp, img, tech }) => (
+            <ProjectCard
+              key={uuid()}
+              id={id}
+              title={title}
+              desc={desc}
+              netlify={netlify}
+              git={git}
+              resp={resp}
+              img={img}
+              tech={tech}
+            />
+          ))}
+        </div>
+      </section>
+      <Connect />
+    </>
   );
 };
 
